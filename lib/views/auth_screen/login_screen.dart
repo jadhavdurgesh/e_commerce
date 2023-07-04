1a4ff6577841b41c3ed2ea899c6bb560af9485ac
DURGESH JADHAV <97460307+Decode749@users.noreply.github.com>
2023-07-05 00:00:11 +0530

    Redirected to Home Screen

--- lib/views/auth_screen/login_screen.dart
+++ lib/views/auth_screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce/consts/consts.dart';
 import 'package:e_commerce/consts/lists.dart';
 import 'package:e_commerce/views/auth_screen/signup_screen.dart';
+import 'package:e_commerce/views/home_screen/home_screen.dart';
 import 'package:e_commerce/widgets%20common/applogo_widget.dart';
 import 'package:e_commerce/widgets%20common/bg_widget.dart';
 import 'package:e_commerce/widgets%20common/button_widget.dart';
@@ -13,83 +14,86 @@ class LoginScreen extends StatelessWidget {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return bgWidget(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Center(
-          child: Column(
-            children: [
-              (context.screenHeight * 0.1 ).heightBox,
-              appLogoWidget(),
-              10.heightBox,
-              "Log into $appname".text.fontFamily(semibold).size(18).white.make(),
-              20.heightBox,
-              Column(
-                children: [
-                  customTextField(
-                    hintText: emailHint,
-                    title: 'Email',
-                    controller: emailController,
-                  ),
-                  customTextField(
-                    hintText: passwordHint,
-                    title: 'Password',
-                    controller: passwordController,
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: (){}, 
-                      child: forgetPassword.text.make()
-                    ),
-                  ),
-                  5.heightBox,
-                  buttonWidget(
-                    title: login,
-                    color: redColor, 
-                    textColor: whiteColor, 
-                    onPress: (){}
-                  ).box.width(context.screenWidth-60).make(),
-                  7.heightBox,
-                  createNewAccount.text.color(fontGrey).make(),
-                  7.heightBox,
-                  buttonWidget(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            appLogoWidget(),
+            10.heightBox,
+            "Log into $appname".text.fontFamily(semibold).size(18).white.make(),
+            20.heightBox,
+            Column(
+              children: [
+                customTextField(
+                  hintText: emailHint,
+                  title: 'Email',
+                  controller: emailController,
+                ),
+                customTextField(
+                  hintText: passwordHint,
+                  title: 'Password',
+                  controller: passwordController,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {}, child: forgetPassword.text.make()),
+                ),
+                5.heightBox,
+                buttonWidget(
+                        title: login,
+                        color: redColor,
+                        textColor: whiteColor,
+                        onPress: () {
+                          Get.to( () => const HomeScreen());
+                        })
+                    .box
+                    .width(context.screenWidth - 60)
+                    .make(),
+                7.heightBox,
+                createNewAccount.text.color(fontGrey).make(),
+                7.heightBox,
+                buttonWidget(
                     title: signup,
-                    color: lightgolden, 
-                    textColor: redColor, 
-                    onPress: (){
-                      Get.to( ()=> SignUpScreen());
-                    }
-                  ).box.width(context.screenWidth-60).make(),
-                  10.heightBox,
-                  loginWith.text.color(fontGrey).make(),
-                  5.heightBox,
-                  Row(
+                    color: lightgolden,
+                    textColor: redColor,
+                    onPress: () {
+                      Get.to(() => const SignUpScreen());
+                    }).box.width(context.screenWidth - 60).make(),
+                10.heightBox,
+                loginWith.text.color(fontGrey).make(),
+                5.heightBox,
+                Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: List.generate(
-                      3, 
-                      (index) => Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: CircleAvatar(
-                          backgroundColor: lightGrey,
-                          child: Image.asset(
-                            socialIconList[index],
-                            width: 35,
-                            ),
-                        ),
-                      )
-                      )
-                  )
-
-                ],
-              ).box.white.rounded.padding(const EdgeInsets.all(16)).width(context.screenWidth-60).shadowMd.make(),
-            ],
-          ),
+                        3,
+                        (index) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: CircleAvatar(
+                                backgroundColor: lightGrey,
+                                child: Image.asset(
+                                  socialIconList[index],
+                                  width: 35,
+                                ),
+                              ),
+                            )))
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(const EdgeInsets.all(16))
+                .width(context.screenWidth - 60)
+                .shadowMd
+                .make(),
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
---
+++ lib/views/home_screen/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: "Home".text.make(),
+      ),
+    );
+  }
+}
