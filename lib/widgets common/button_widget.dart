1ccaf728522f35277adb8163ce86bca9778608c5
DURGESH JADHAV <97460307+Decode749@users.noreply.github.com>
2023-07-09 22:42:53 +0530

    done item details

--- lib/widgets common/button_widget.dart
+++ lib/widgets common/button_widget.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce/consts/consts.dart';
 Widget buttonWidget({onPress, color, textColor,String? title}){
   return ElevatedButton(
     style: ElevatedButton.styleFrom(
+      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(4)),
       backgroundColor: color,
       padding: const EdgeInsets.all(12)
     ),
